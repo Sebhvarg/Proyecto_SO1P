37d17e57702c1586c917fd42be77aa13b51a99f7
Sebhvarg <sebhvarg@espol.edu.ec>
2025-12-29 22:19:59 -0500

    broker

--- broker.c
+++ broker.c
@@ -5,82 +5,107 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <time.h>
+#include "time.h"
 #include "gateway.h"
 
-message_t msg[MAX_CLIENTS];
-int msg_count = 0;
 pthread_mutex_t msg_mutex;
 subscriber_t *subscribers_head = NULL;
 pthread_mutex_t subscribers_mutex;
 
-void initialize_broker() {
+void initialize_broker()
+{
     int server_socket, new_socket;
     struct sockaddr_in server_addr;
     int addr_len = sizeof(server_addr);
     pthread_t thread_id;
-    pthread_mutex_init(&msg_mutex, NULL);
+
+    pthread_mutex_init(&log_mutex, NULL);
     pthread_mutex_init(&subscribers_mutex, NULL);
 
-    // Crear el socket del servidor
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Creacion del socket fallida");
-        exit(EXIT_FAILURE);
-    }
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-    // Enlazar el socket
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Fallo en el bind");
-        close(server_socket);
+    server_addr.sin_port = htons(port);
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error en bind");
         exit(EXIT_FAILURE);
     }
-    // Escuchar conexiones entrantes
-    if (listen(server_socket, MAX_CLIENTS) < 0) {
-        perror("Fallo en el listen");
-        close(server_socket);
+    if (listen(server_socket, 3) < 0)
+    {
+        perror("Error en listen");
         exit(EXIT_FAILURE);
     }
-    printf("Broker escuchando en el puerto %d\n", PORT);
+    printf("Broker escuchando en el puerto %d\n", port);
+
     while (1)
     {
-        if ((new_socket = accept(server_socket, (struct sockaddr *)&server_addr, (socklen_t*)&addr_len))<0) {
-            perror("Fallo en el accept");
+        new_socket = accept(server_socket, (struct sockaddr *)&server_addr, (socklen_t *)&addr_len);
+        if (new_socket < 0)
+        {
+            perror("Error en accept");
             continue;
         }
-        int *client_socket = malloc(sizeof(int));
-        *client_socket = new_socket;
-
-        if (pthread_create(&thread_id, NULL, handle_client, (void *)client_socket) != 0) {
-            perror("Fallo en la creacion del hilo");
-            free(client_socket);
+        printf("Nuevo cliente conectado\n");
+        int *pclient = malloc(sizeof(int));
+        *pclient = new_socket;
+        pthread_create(&thread_id, NULL, handle_client, pclient);
+        pthread_detach(thread_id);
+    }
+}
+int match_topic(const char *subscribed_topics, const char *topic)
+{
+    if (strstr(subscribed_topics, "#") != NULL)
+    {
+        size_t len = strlen(subscribed_topics);
+        if (len > 2 && subscribed_topics[len - 2] == '/' && subscribed_topics[len - 1] == '#')
+        {
+            char base_topic[MAX_TOPICS];
+            strncpy(base_topic, subscribed_topics, len - 2);
+            base_topic[len - 2] = '\0';
+            if (strncmp(topic, base_topic, strlen(base_topic)) == 0)
+            {
+                if (topic[strlen(base_topic)] == '/' || topic[strlen(base_topic)] == '\0')
+                {
+                    return 1;
+                }
+            }
         }
-        pthread_detach(thread_id); // Desvincular el hilo para que libere recursos al terminar
+        return 0;
     }
-    
+    return strcmp(subscribed_topics, topic) == 0;
+}
+void save_message_log(const char *topic, const char *value)
+{
+    pthread_mutex_lock(&log_mutex);
+    FILE *log_file = fopen(MSG_LOG_FILE, "a");
+    if (log_file != NULL)
+    {
+        time_t now = time(NULL);
+        char timestamp[30];
+        strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", localtime(&now));
+        fprintf(log_file, "[%s] Topic: %s, Value: %s\n", timestamp, topic, value);
+        fclose(log_file);
+    }
+    else
+    {
+        perror("Error al abrir el archivo de log");
+    }
+    pthread_mutex_unlock(&log_mutex);
 }
 
-void notify_subscribers(const char *topic, const char *value);
-
-void publish_message(const char *topic, const char *value) {
-    pthread_mutex_lock(&msg_mutex);
-    if (msg_count < MAX_CLIENTS) {
-        strncpy(msg[msg_count].topic, topic, MAX_TOPICS);
-        strncpy(msg[msg_count].value, value, MAX_VALUE_LENGTH);
-        msg_count++;
-        printf("Mensaje publicado en el broker: Topic: %s, Value: %s\n", topic, value);
-    } else {
-        printf("Broker lleno, no se puede publicar mas mensajes\n");
-    }
-    pthread_mutex_unlock(&msg_mutex);
-    notify_subscribers(topic, value);
-}
-
-void notify_subscribers(const char *topic, const char *value) {
+void notify_subscribers(const char *topic, const char *value)
+{
     pthread_mutex_lock(&subscribers_mutex);
     subscriber_t *current = subscribers_head;
-    while (current != NULL) {
-        if (strstr(current->topics, topic) != NULL) {
+    while (current != NULL)
+    {
+        if (match_topic(current->topics, topic))
+        {
             send(current->socket, value, strlen(value), 0);
         }
         current = current->next;
@@ -88,31 +113,49 @@ void notify_subscribers(const char *topic, const char *value) {
     pthread_mutex_unlock(&subscribers_mutex);
 }
 
-void* handle_client(void *arg) {
+void publish_message(const char *topic, const char *value)
+{
+    printf("Publicando mensaje en el tema: %s, valor: %s\n", topic, value);
+    save_message_log(topic, value);
+    char notify_buffer[BUFFER_SIZE];
+    snprintf(notify_buffer, BUFFER_SIZE, "Nuevo mensaje en %s: %s", topic, value);
+    notify_subscribers(topic, value);
+}
+
+void *handle_client(void *arg)
+{
     int client_socket = *((int *)arg);
     free(arg);
     char buffer[BUFFER_SIZE];
-    char topic[MAX_TOPICS];
-    char value[MAX_VALUE_LENGTH];
-
-    while (1) {
+    while (1)
+    {
         memset(buffer, 0, BUFFER_SIZE);
-        int bytes_read = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
-        if (bytes_read <= 0) {
+        int bytes_received = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+        if (bytes_received <= 0)
+        {
             printf("Cliente desconectado\n");
             close(client_socket);
             return NULL;
         }
-        buffer[bytes_read] = '\0';
-
-        if (strncmp(buffer, "PUBLISH", 7) == 0) {
-            sscanf(buffer + 8, "%s %s", topic, value);
-            publish_message(topic, value);
-        } else if (strncmp(buffer, "SUBSCRIBE", 9) == 0) {
-            sscanf(buffer + 10, "%s", topic);
-            subscriber_t *new_subscriber = malloc(sizeof(subscriber_t));
+        buffer[bytes_received] = '\0';
+        printf("Mensaje recibido: %s\n", buffer);
+        if (strncmp(buffer, "PUBLICADO:", 10) == 0)
+        {
+            char *p = buffer + 10;
+            char *topic = strtok(p, ":");
+            char *value = strtok(NULL, ":");
+            if (topic != NULL && value != NULL)
+            {
+                publish_message(topic, value);
+            }
+        }
+        else if (strncmp(buffer, "SUSCRIBIR:", 10) == 0)
+        {
+            char topics[MAX_TOPICS];
+            sscanf(buffer + 10, "%s", topics);
+            subscriber_t *new_subscriber = (subscriber_t *)malloc(sizeof(subscriber_t));
             new_subscriber->socket = client_socket;
-            strncpy(new_subscriber->topics, topic, MAX_TOPICS);
+            strncpy(new_subscriber->topics, topics, MAX_TOPICS);
             new_subscriber->next = NULL;
 
             pthread_mutex_lock(&subscribers_mutex);
@@ -120,9 +163,19 @@ void* handle_client(void *arg) {
             subscribers_head = new_subscriber;
             pthread_mutex_unlock(&subscribers_mutex);
 
-            printf("Cliente suscrito al tema: %s\n", topic);
+            printf("Cliente suscrito a los temas: %s\n", topics);
         }
     }
     close(client_socket);
     return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    int port = PORT;
+    if(argc > 1) {
+        port = atoi(argv[1]);
+    }
+    initialize_broker();
+    return 0;
 }
--- broker.h
+++ broker.h
@@ -2,21 +2,22 @@
 #define BROKER_H
 #include <pthread.h>
 
-#define PORT 8080
 #define MAX_CLIENTS 100
 #define BUFFER_SIZE 2048
-#define MAX_VALUE_LENGTH 50
-#define MAX_TOPICS 50
+#define MAX_VALUE_LENGTH 256
+#define MAX_TOPICS 100
+#define MSG_LOG_FILE "messages.log"
+#define PORT 8080
 
 typedef struct {
     char topic[MAX_TOPICS];
     char value[MAX_VALUE_LENGTH];
+    char tiemestamp[30];
 } message_t;
 
 typedef struct subscriber {
     int socket;
     char topics[MAX_TOPICS];
-    
     struct subscriber *next;
 } subscriber_t;
 
